ce6048cc81ae024b4e1f1204c5ece5430a312fe3
Mert Duman <duman.m.mert@gmail.com>
2023-04-11 22:05:40 +0300

    Added bias control to attention blocks.

--- sonn/building_blocks/attention_blocks.py
+++ sonn/building_blocks/attention_blocks.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 
 class SimpleChannelAttention2d(nn.Module):
     ''' A wrapper for AvgChannelAttention2d. '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0)
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, bias=bias)
 
     def forward(self, x):
         return self.conv(self.avgpool(x))
@@ -20,11 +20,11 @@ class AvgChannelAttention2d(nn.Module):
     The average channel attention module from https://arxiv.org/abs/1807.06521. 
     The output is a channel attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0)
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, bias=bias)
 
     def forward(self, x):
         return self.conv(self.avgpool(x))
@@ -35,11 +35,11 @@ class MaxChannelAttention2d(nn.Module):
     The max channel attention module from https://arxiv.org/abs/1807.06521. 
     The output is a channel attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
         self.maxpool = nn.AdaptiveMaxPool2d(1)
-        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0)
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, bias=bias)
 
     def forward(self, x):
         return self.conv(self.maxpool(x))
@@ -50,12 +50,12 @@ class AvgMaxChannelAttention2d(nn.Module):
     The average + max channel attention module from https://arxiv.org/abs/1807.06521. 
     The output is a channel attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.maxpool = nn.AdaptiveMaxPool2d(1)
-        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0)
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, bias=bias)
 
     def forward(self, x):
         return self.conv(self.avgpool(x) + self.maxpool(x))
@@ -66,10 +66,10 @@ class AvgSpatialAttention2d(nn.Module):
     The average spatial attention module from https://arxiv.org/abs/1807.06521. 
     The output is a spatial attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
-        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1)
+        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1, bias=bias)
 
     def forward(self, x):
         return self.conv(torch.mean(x, dim=1, keepdim=True))
@@ -80,10 +80,10 @@ class MaxSpatialAttention2d(nn.Module):
     The max spatial attention module from https://arxiv.org/abs/1807.06521. 
     The output is a spatial attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
-        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1)
+        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1, bias=bias)
 
     def forward(self, x):
         return self.conv(torch.amax(x, dim=1, keepdim=True))
@@ -94,10 +94,10 @@ class AvgMaxSpatialAttention2d(nn.Module):
     The average + max spatial attention module from https://arxiv.org/abs/1807.06521. 
     The output is a spatial attention map that is generally multiplied with the input.
     '''
-    def __init__(self, dim):
+    def __init__(self, dim, bias=True):
         super().__init__()
 
-        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1)
+        self.conv = nn.Conv2d(dim, 1, kernel_size=3, padding=1, bias=bias)
 
     def forward(self, x):
         return self.conv(torch.mean(x, dim=1, keepdim=True) + torch.amax(x, dim=1, keepdim=True))
