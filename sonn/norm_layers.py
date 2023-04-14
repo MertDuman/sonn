a762c72981812fb344b4f5b7abad55354e6dbce8
Mert Duman <duman.m.mert@gmail.com>
2023-04-14 14:23:12 +0300

    LayerNorm 1d

--- sonn/norm_layers.py
+++ sonn/norm_layers.py
@@ -22,6 +22,25 @@ class LayerNormNLP2d(nn.Module):
         return x
     
 
+class LayerNormNLP1d(nn.Module):
+    ''' 
+    The name NLP implies that this layer norm does not normalize the C x H x W dimensions (default), but only the C dimension (NLP). 
+    https://i.stack.imgur.com/1JdN6.png
+
+    This is useful for NLP tasks, where the input is a sequence of word embeddings, and the C dimension is the embedding dimension.
+    But it has also been used in image tasks, where the C dimension is the number of channels.
+    '''
+    def __init__(self, channels, affine=True):
+        super().__init__()
+        self.layer_norm = nn.LayerNorm(channels, elementwise_affine=affine)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)  # n c d -> n d c
+        x = self.layer_norm(x)
+        x = x.permute(0, 2, 1)  # n d c -> n c d
+        return x
+    
+
 class InstanceNormalize2d(nn.Module):
     ''' Normalize each channel of each image separately with min-max normalization. '''
     def __init__(self, affine=False):
